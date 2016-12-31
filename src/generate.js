d20738cb8a2ce7dfcdae7f78fd207a1e6adb7581
Michael Ferris <mike.ferris@hotmail.com>
2016-12-31 15:01:28 -0800

    Add parameters to the generate function

--- src/generate.js
+++ src/generate.js
@@ -1,66 +1,91 @@
-import {outputDir, toolsDirectory} from "./init";
+import {outputDir as defaultOutdir, toolsDirectory} from "./init";
 import fs from "fs-extra";
 import path from "path";
 import {spawn} from "child_process";
 import {generateDependencies} from "./dependencies";
 import {waitUntilDone} from "./utils";
 
-/*
 export const sourceTypes = {
   c: Symbol(),
   cpp: Symbol(),
   cpp11: Symbol()
-}
-*/
+};
 
 export default async function generate({
-  //sourceType = "c"
-}, execOptions = {}) {
+  sourceType = sourceTypes.c,
+  validate = true,
+  interpreted = true,
+  srcFileName = "test",
+  wasmFileName = "test",
+  interpretedFileName = "interpreted",
+  outdir = defaultOutdir,
+  execOptions = {},
+} = {}) {
   const {csmith, wasm, runEmcc} = await generateDependencies();
 
-  await fs.ensureDirAsync(outputDir);
+  await fs.ensureDirAsync(outdir);
+
   // Generate random c file
-  const csmithProc = spawn(csmith, ["-o", "test.c"], {
+  let sourceFile = path.resolve(outdir, srcFileName);
+  const csmithArgs = [];
+
+  switch (sourceType) {
+    case sourceTypes.c:
+      sourceFile += ".c";
+      break;
+    case sourceTypes.cpp11:
+      csmithArgs.push("--cpp11");
+      // Fallthrough
+    case sourceTypes.cpp:
+      sourceFile += ".cpp";
+      csmithArgs.push("--lang-cpp");
+      break;
+    default: throw new Error("Unknown source type");
+  }
+  const csmithProc = spawn(csmith, ["-o", sourceFile, ...csmithArgs], {
     stdio: "inherit",
-    cwd: outputDir,
+    cwd: outdir,
     ...execOptions
   });
   await waitUntilDone(csmithProc);
 
-  /*
-  await execFileAsync(csmithExe, ["-o", "test.cpp", "--lang-cpp"], {
-    cwd: outputDir
-  });
-  await execFileAsync(csmithExe, ["-o", "test11.cpp", "--lang-cpp", "--cpp11"], {
-    cwd: outputDir
-  });
-  */
-
   // Generate wasm version
+  const emccCommonArgs = [
+    sourceFile,
+    `-I${path.join(toolsDirectory, "csmith/inc")}`,
+    "-O3",
+  ];
+  const wasmFile = path.resolve(outdir, `${wasmFileName}.js`);
   await runEmcc([
-    "test.c",
-    `-I${path.relative(outputDir, path.join(toolsDirectory, "csmith/inc"))}`,
-    ..."-O3 -s WASM=1 -o test.js".split(" "),
-  ], {cwd: outputDir, ...execOptions});
+    ...emccCommonArgs,
+    "-s", "WASM=1",
+    "-o", wasmFile
+  ], {cwd: outdir, ...execOptions});
 
   // Make sure it is valid
   // Emscripten sometimes generates invalid wasm file, should investigate
-  const specProc = spawn(wasm, [path.resolve(outputDir, "test.wasm"), "-d"], {
-    cwd: outputDir,
-    ...execOptions
-  });
-  await waitUntilDone(specProc);
+  if (validate) {
+    const specProc = spawn(wasm, [path.resolve(outdir, "test.wasm"), "-d"], {
+      cwd: outdir,
+      ...execOptions
+    });
+    await waitUntilDone(specProc);
+  }
 
   // Generate interpreted version to compare result
-  await runEmcc([
-    "test.c",
-    `-I${path.relative(outputDir, path.join(toolsDirectory, "csmith/inc"))}`,
-    ..."-O3 -s BINARYEN_METHOD='interpret-binary' -o interpret.js".split(" "),
-  ], {cwd: outputDir, ...execOptions});
+  let interpretedFile = null;
+  if (interpreted) {
+    interpretedFile = path.resolve(outdir, `${interpretedFileName}.js`);
+    await runEmcc([
+      ...emccCommonArgs,
+      "-s", "BINARYEN_METHOD='interpret-binary'",
+      "-o", interpretedFile
+    ], {cwd: outdir, ...execOptions});
+  }
 
   return {
-    src: path.resolve(outputDir, "test.c"),
-    wasm: path.resolve(outputDir, "test.js"),
-    interpret: path.resolve(outputDir, "interpret.js"),
+    src: sourceFile,
+    wasm: wasmFile,
+    interpret: interpretedFile,
   };
 }
