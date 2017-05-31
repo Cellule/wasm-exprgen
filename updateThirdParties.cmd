8bc32eb374254dc4ffaf2347e5ecc46493e7afcc
Michael Ferris <micfer@microsoft.com>
2017-05-30 17:01:51 -0700

    Update third parties

--- updateThirdParties.cmd
+++ updateThirdParties.cmd
@@ -2,17 +2,22 @@ pushd %~dp0
 call git submodule update --init
 cd third_party\clang
 call git fetch
-call git checkout origin/incoming
+call git clean -fdx
+call git reset --hard origin/incoming
 cd ..\emscripten
 call git fetch
-call git checkout origin/incoming
+call git clean -fdx
+call git reset --hard origin/incoming
 cd ..\emscripten-fastcomp
 call git fetch
-call git checkout origin/incoming
+call git clean -fdx
+call git reset --hard origin/incoming
 cd ..\csmith
 call git fetch
-call git checkout origin/master
+call git clean -fdx
+call git reset --hard origin/master
 cd ..\wasm-spec
 call git fetch
-call git checkout origin/master
+call git clean -fdx
+call git reset --hard origin/master
 popd
